583f91dd2ff0d0aec352208171477e16b2c45095
LacYCle <wuiyilang516@gmail.com>
2025-11-12 14:22:25 +0800

    1
    1

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,8 @@
-var Score = artifacts.require("./Score.sol");
+const ConvertLib = artifacts.require("ConvertLib");
+const MetaCoin = artifacts.require("MetaCoin");
+
 module.exports = function(deployer) {
-  deployer.deploy(Score);
+  deployer.deploy(ConvertLib);
+  deployer.link(ConvertLib, MetaCoin);
+  deployer.deploy(MetaCoin);
 };
